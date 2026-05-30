5dd8bff6c656e02d2c2fddc9290b925f05de5bee
pouria :) <pouriapmd@gmail.com>
2026-05-30 17:34:54 +0330

    implemented local hill climbing

--- search/hill_climbing.py
+++ search/hill_climbing.py
@@ -1,17 +1,16 @@
 from search.local_search_base import LocalSearchBase
 
-
 class HillClimbing(LocalSearchBase):
     def run(self, initial_state, **kwargs):
         """
-        TODO: Implement the Hill Climbing algorithm.
-        
+        ~~TODO: Implement the Hill Climbing algorithm.~~ done:)
+
         Parameters
         ----------
         initial_state : list of tuples
             The initial configuration of sensors.
-        **kwargs : 
-            Define and add any other parameters you might need for the algorithm 
+        **kwargs :
+            Define and add any other parameters you might need for the algorithm
 
         Returns
         -------
@@ -24,5 +23,42 @@ class HillClimbing(LocalSearchBase):
         states_history : list of lists
             List of states at each iteration (used for animation).
         """
-        
-        raise NotImplementedError("Students must implement this method.")
+
+        # get settings or fall back to defaults
+        max_iterations = kwargs.get("max_iterations", 1000)
+        max_no_improve = kwargs.get("max_no_improve", 100)
+
+        current_state = list(initial_state) if initial_state is not None else []
+        current_cost = self.evaluate(current_state)
+        best_state = list(current_state)
+        best_cost = current_cost
+
+        # history
+        evaluations = [current_cost]
+        states_history = [list(current_state)]
+        no_improve_count = 0
+
+        for _ in range(max_iterations):
+            neighbour = self.get_neighbor(current_state)
+            neighbour_cost = self.evaluate(neighbour)
+
+            evaluations.append(neighbour_cost)
+            states_history.append(list(neighbour))
+
+            # accepting better neighbors
+            if neighbour_cost < current_cost:
+                current_state = list(neighbour)
+                current_cost = neighbour_cost
+                no_improve_count = 0
+                # update best state
+                if neighbour_cost < best_cost:
+                    best_cost = neighbour_cost
+                    best_state = list(neighbour)
+            else:
+                no_improve_count += 1
+
+            if no_improve_count >= max_no_improve:
+                break
+
+        return best_state, best_cost, evaluations, states_history
+
